1ca4046ef020009db4be6c03239360745bfdf509
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-07-08 20:30:55 +0200

    Commit

--- src/main/java/Lesson10/Task3/UrlValidator.java
+++ src/main/java/Lesson10/Task3/UrlValidator.java
@@ -3,15 +3,13 @@ package Lesson10.Task3;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class Url {
+public class UrlValidator {
+
     public static void main(String[] args) {
-        Pattern pattern = Pattern.compile("((http|https)://)(www.)?"
-                + "[a-zA-Z0-9@:%._\\+~#?&//=]"
-                + "{2,256}\\.[a-z]"
-                + "{2,6}\\b([-a-zA-Z0-9@:%"
-                + "._\\+~#?&//=]*)");
+        Pattern pattern = Pattern.compile("^(http:\\/\\/www\\.|https:\\/\\/www\\.|http:\\/\\/" +
+                "|https:\\/\\/)?[a-z0-9]+([\\-\\.]{1}[a-z0-9]+)*\\.[a-z]{2,5}(:[0-9]{1,5})?(\\/.*)?$");
         Matcher matcher1 = pattern.matcher("http://www.onet.pl");
-        Matcher matcher2 = pattern.matcher("https://mail.google.com");
+        Matcher matcher2 = pattern.matcher("https://mailgoogle.com");
         Matcher matcher3 = pattern.matcher("http://wiadmosci.onet.pl");
         Matcher matcher4 = pattern.matcher(" http://onet.pl");
 
@@ -19,5 +17,11 @@ public class Url {
         boolean matches2 = matcher2.find();
         boolean matches3 = matcher3.find();
         boolean matches4 = matcher4.find();
+
+        System.out.println(matches1);
+        System.out.println(matches2);
+        System.out.println(matches3);
+        System.out.println(matches4);
     }
+
 }
