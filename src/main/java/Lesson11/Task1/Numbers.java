71879d38f3fde255129ad9a687229daa0efe4771
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-07-08 21:48:45 +0200

    Task1

---
+++ src/main/java/Lesson11/Task1/Numbers.java
@@ -0,0 +1,22 @@
+package Lesson11.Task1;
+
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+
+public class Numbers {
+    public static void main(String[] args) {
+
+
+        IntStream numbers = IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        numbers
+                .filter(IntStream -> (IntStream % 3) == 0)
+                .boxed()
+                .collect(Collectors.toList())
+                .forEach(System.out::println);
+
+    }
+
+}
+
