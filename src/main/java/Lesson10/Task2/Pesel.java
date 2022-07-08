9aaf8c133d93421d565b1becf0c5ab3d4d2c776c
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-07-08 09:51:17 +0200

    Commit

--- src/main/java/Lesson10/Task2/Pesel.java
+++ src/main/java/Lesson10/Task2/Pesel.java
@@ -5,16 +5,12 @@ import java.util.regex.Pattern;
 
 public class Pesel {
 
-    public void checkLength(CharSequence Pesel) {
-        Pattern pattern = Pattern.compile("\\d\\d\\d\\d\\d\\d\\d\\d\\d");
+    public static void validatePesel(CharSequence Pesel){
+        Pattern pattern = Pattern.compile("^\\d{11}$");
         Matcher matcher = pattern.matcher(Pesel);
         boolean matches = matcher.matches();
     }
 
-    public void checkPropiety(CharSequence Pesel) {
-        Pattern pattern = Pattern.compile(".*\\d.*");
-        Matcher matcher = pattern.matcher(Pesel);
-        boolean matches = matcher.matches();
     }
 
-}
+
