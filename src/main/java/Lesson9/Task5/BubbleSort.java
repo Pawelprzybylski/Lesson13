cf31749dd0fa020819accd4b1bf9559e9652008b
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-07-12 14:43:18 +0200

    Commit

--- src/main/java/Lesson9/Task5/BubbleSort.java
+++ src/main/java/Lesson9/Task5/BubbleSort.java
@@ -2,13 +2,13 @@ package Lesson9.Task5;
 
 public class BubbleSort<T> {
 
-    public T[] bubbleSort(Integer[] elements) {
+    public T[] bubbleSort(T[] elements) {
         for (int i = 0; i < elements.length; i++) {
             for (int j = 0; j < elements.length - 1; j++) {
                 if (elements[j] > elements[j + 1]) {
-                    Integer temporary = elements[j + 1];
+                    Integer temporary = (Integer) elements[j + 1];
                     elements[j + 1] = elements[j];
-                    elements[j] = temporary;
+                    elements[j] = (T) temporary;
                 }
             }
         }
