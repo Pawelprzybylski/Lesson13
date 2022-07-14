aa0bd335d133f40b97d2e2d7a57485f41604dfea
Pawel Przybylski <pawel.przybylski1989@gmail.com>
2022-07-14 18:03:36 +0200

    Commit

--- src/main/java/Lesson9/Task5/BubbleSort.java
+++ src/main/java/Lesson9/Task5/BubbleSort.java
@@ -1,18 +1,18 @@
 package Lesson9.Task5;
 
-public class BubbleSort<T> {
+public class BubbleSort {
 
-    public T[] bubbleSort(T[] elements) {
-        for (int i = 0; i < elements.length; i++) {
-            for (int j = 0; j < elements.length - 1; j++) {
-                if (elements[j] > elements[j + 1]) {
-                    Integer temporary = (Integer) elements[j + 1];
-                    elements[j + 1] = elements[j];
-                    elements[j] = (T) temporary;
-                }
-            }
-        }
-        return elements;
-    }
+  public static <T extends Comparable<T>> T[] bubbleSort(T[] elements) {
+      for (int i = 0; i < elements.length; i++) {
+          for (int j = 0; j < elements.length - 1; j++) {
+              if (elements[j].compareTo(elements[j + 1]) > 0) {
+                  T temporary = elements[j + 1];
+                  elements[j + 1] = elements[j];
+                  elements[j] = temporary;
+              }
+          }
+      }
+      return elements;
+  }
 
 }
